11eba2bbd56775e25f88084b4b369926856fd241
Jeanie Chung <jeanie.chung@gmail.com>
2015-02-19 19:05:25 -0800

    Objectifying rainbow ipsum and added markup for nsfw option.

--- dev/main.js
+++ dev/main.js
@@ -1,240 +1,271 @@
-'use strict'
+(function() {
+  'use strict'
 
-
-var gayz = [
-  "Shane",
-  "Alice",
-  "unicorns",
-  "scissoring",
-  "Tegan",
-  "Sara",
-  "mullet",
-  "uhaul",
-  "family",
-  "cat",
-  "Gloria Steinem",
-  "feminist",
-  "femme",
-  "butch",
-  "chapstick",
-  "rainbow",
-  "glitter",
-  "Broadway",
-  "Ilene Chaiken",
-  "Bette",
-  "Tina",
-  "Helena",
-  "Jenny",
-  "crying",
-  "laughing",
-  "loving",
-  "fighting",
-  "kissing",
-  "processing",
-  "feelings",
-  "feeling",
-  "Carmen",
-  "Robyn",
-  "Dana",
-  "Max",
-  "Marina",
-  "Tasha",
-  "boi",
-  "alternative lifestyle haircut",
-  "tattoos",
-  "pillow queen",
-  "Dinah Shore",
-  "boobs",
-  "sports-bra",
-  "pride",
-  "rainbow",
-  "glitter",
-  "queer",
-  "gaycation",
-  "beard",
-  "queer",
-  "gay",
-  "bi",
-  "trans",
-  "pansexual",
-  "LGBTQIA",
-  "gaybies",
-  "drag king",
-  "drag queen",
-  "RuPaul",
-  "drag show",
-  "gay bar",
-  "Stonewall",
-  "gay",
-  "ghey",
-  "top",
-  "bottom",
-  "spaghetti",
-  "heteroflexible",
-  "Britney",
-  "Christina",
-  "Lady Gaga",
-  "Madonna",
-  "Queer As Folk",
-  "sweeps week",
-  "Jenny Owens Young",
-  "twink",
-  "hard femme",
-  "tomboy",
-  "leather",
-  "bear",
-  "bug chaser",
-  "rent boy",
-  "gurl",
-  "Emmett",
-  "Michael",
-  "Brian",
-  "Kinney",
-  "Looking",
-  "GLAAD",
-  "Patrick",
-  "Dom",
-  "Augustine",
-  "Dorris",
-  "Richie",
-  "agenda",
-  "Happy Holigays",
-  "Rosie",
-  "Ellen",
-  "DeGeneres",
-  "Page",
-  "gaydar",
-  "lesbian",
-  "down-low",
-  "Nicki Minaj",
-  "Beyoncé",
-  "Bette Midler",
-  "Cher",
-  "Laverne Cox",
-  "Janet Mock",
-  "brunch",
-  "veganism",
-  "flannel",
-  "slouchy beanies",
-  "combat boots",
-  "Harvey Milk",
-  "gender",
-  "androgyny",
-  "Liza Minnelli",
-  "Judy Garland",
-  "campy",
-  "genderqueer",
-  "Meryl Streep",
-  "Neil Patrick Harris",
-  "George Takei",
-  "rugby",
-  "softball",
-  "Megan Rapinoe",
-  "Cyndi Lauper",
-  "Melissa Etheridge",
-  "Subaru",
-  "Castro",
-  "Boystown",
-  "triangle",
-  "Trevor Project",
-  "GSA",
-  "PFLAG",
-  "marriage equality",
-  "dapper",
-  "bowtie",
-  "tacos",
-  "fish",
-  "engaygement",
-  "you\'re a wanker number 9",
-  "quinoa"
-];
-
-var dirty = [
-  "fisting",
-  "dildo",
-  "fucking",
-  "strap-on",
-  "fuckin' Jenny",
-  "rimjob"
-];
-
-
-var randInt = function(num) {
-  return Math.floor((Math.random() * num));
-};
-
-var processing = function() {
-  // processing stuff here lulz
-
-  function eliminateDuplicates(arr) {
-    var i,
-        len=arr.length,
-        out=[],
-        obj={};
-
-    for (i=0;i<len;i++) {
-      obj[arr[i]]=0;
-    }
-    for (i in obj) {
-      out.push(i);
-    }
-    return out;
-  }
-};
-
-var punctuation = function() {
-  var characters = ['.','!','?'];
-
-  var x = randInt(characters.length);
-  return characters[x];
-};
-
-var makeSentence = function(numWords) {
-  var words = [];
-
-  for (var i = 0; i < numWords; i++) {
-    words.push(gayz[randInt(gayz.length)]);
+  var gayGenerator = function gayGenerator() {
+    this.nsfw = false,
+    this.gayz = [
+      "Shane",
+      "Alice",
+      "unicorns",
+      "scissoring",
+      "Tegan",
+      "Sara",
+      "mullet",
+      "uhaul",
+      "family",
+      "cat",
+      "Gloria Steinem",
+      "feminist",
+      "femme",
+      "butch",
+      "chapstick",
+      "rainbow",
+      "glitter",
+      "Broadway",
+      "Ilene Chaiken",
+      "Bette",
+      "Tina",
+      "Helena",
+      "Jenny",
+      "crying",
+      "laughing",
+      "loving",
+      "fighting",
+      "kissing",
+      "processing",
+      "feelings",
+      "feeling",
+      "Carmen",
+      "Robyn",
+      "Dana",
+      "Max",
+      "Marina",
+      "Tasha",
+      "boi",
+      "alternative lifestyle haircut",
+      "tattoos",
+      "pillow queen",
+      "Dinah Shore",
+      "boobs",
+      "sports-bra",
+      "pride",
+      "rainbow",
+      "glitter",
+      "queer",
+      "gaycation",
+      "beard",
+      "queer",
+      "gay",
+      "bi",
+      "trans",
+      "pansexual",
+      "LGBTQIA",
+      "gaybies",
+      "drag king",
+      "drag queen",
+      "RuPaul",
+      "drag show",
+      "gay bar",
+      "Stonewall",
+      "gay",
+      "ghey",
+      "top",
+      "bottom",
+      "spaghetti",
+      "heteroflexible",
+      "Britney",
+      "Christina",
+      "Lady Gaga",
+      "Madonna",
+      "Queer As Folk",
+      "sweeps week",
+      "Jenny Owens Young",
+      "twink",
+      "hard femme",
+      "tomboy",
+      "leather",
+      "bear",
+      "bug chaser",
+      "rent boy",
+      "gurl",
+      "Emmett",
+      "Michael",
+      "Brian",
+      "Kinney",
+      "Looking",
+      "GLAAD",
+      "Patrick",
+      "Dom",
+      "Augustine",
+      "Dorris",
+      "Richie",
+      "agenda",
+      "Happy Holigays",
+      "Rosie",
+      "Ellen",
+      "DeGeneres",
+      "Page",
+      "gaydar",
+      "lesbian",
+      "down-low",
+      "Nicki Minaj",
+      "Beyoncé",
+      "Bette Midler",
+      "Cher",
+      "Laverne Cox",
+      "Janet Mock",
+      "brunch",
+      "veganism",
+      "flannel",
+      "slouchy beanies",
+      "combat boots",
+      "Harvey Milk",
+      "gender",
+      "androgyny",
+      "Liza Minnelli",
+      "Judy Garland",
+      "campy",
+      "genderqueer",
+      "Meryl Streep",
+      "Neil Patrick Harris",
+      "George Takei",
+      "rugby",
+      "softball",
+      "Megan Rapinoe",
+      "Cyndi Lauper",
+      "Melissa Etheridge",
+      "Subaru",
+      "Castro",
+      "Boystown",
+      "triangle",
+      "Trevor Project",
+      "GSA",
+      "PFLAG",
+      "marriage equality",
+      "dapper",
+      "bowtie",
+      "tacos",
+      "fish",
+      "engaygement",
+      "you\'re a wanker number 9",
+      "quinoa"
+    ],
+    this.dirty = [
+      "fisting",
+      "dildo",
+      "fucking",
+      "strap-on",
+      "fuckin' Jenny",
+      "rimjob"
+    ];
   }
 
-  var string = words.join().replace(/,/g , ' ');
-  console.log(string[0]);
-  string = string[0].toUpperCase() + string.slice(1) + punctuation();
-  return string;
-};
 
-var makeParagraph = function(numSentence, numWords) {
-  var sentences = [];
-  var maxSentenceLength = numWords || 10;
-  var sentenceLength = randInt(maxSentenceLength);
-  for (var i = 0; i < numSentence; i++) {
-    sentences.push(makeSentence(sentenceLength));
+  gayGenerator.prototype = {
+
+    // get a random number (non-zero)
+    randInt: function(num) {
+      return Math.floor((Math.random() * num) + 1);
+    },
+
+    // processing stuff here lulz
+    processing: function() {
+      function eliminateDuplicates(arr) {
+        var i,
+            len=arr.length,
+            out=[],
+            obj={};
+
+        for (i=0;i<len;i++) {
+          obj[arr[i]]=0;
+        }
+        for (i in obj) {
+          out.push(i);
+        }
+        return out;
+      }
+    },
+
+    // have yourself some punctuation
+    punctuation: function() {
+      var characters = ['.','!','?'];
+      console.log(this);
+      var x = this.randInt(characters.length);
+      return characters[x];
+    },
+
+    // make a gay ass sentence
+    makeSentence: function(numWords) {
+      var words = [],
+          self  = this;
+
+      var gayz  = self.gayz;
+
+      for (var i = 0; i < numWords; i++) {
+        words.push(gayz[self.randInt(gayz.length)]);
+      }
+
+      var string = words.join().replace(/,/g , ' ');
+
+      // Upper case dat trick and add some radical punctuation!?
+
+      string = string[0].toUpperCase() + string.slice(1) + self.punctuation();  
+
+      return string;
+    },
+    makeParagraph: function(numSentence, numWords) {
+      var sentences         = [],
+          maxSentenceLength = numWords || 10,
+          self              = this;
+
+      var sentenceLength = self.randInt(maxSentenceLength)
+
+      for (var i = 0; i < numSentence; i++) {
+        sentences.push(self.makeSentence(sentenceLength));
+      }
+
+      var string = sentences.join().replace(/,/g , ' ');;
+      return string;
+    }
   }
 
-  // something like this;
-  var string = sentences.join().replace(/,/g , ' ');;
-  return string;
-};
+  // DOM stuff
+  $(document).on('ready', function() {
 
+    // ooo pretty rainbows
+    var randomColors = ['#FF0036','#FFAA00','#dfdf04','#BCE400','#0080FF','#FF007F','#7A00E5','#FF00FF']
+    var r = Math.floor((Math.random()*randomColors.length));
+    $('body').css('background-color', randomColors[r]);
 
-$(document).on('ready', function() {
-  $('#gay-generator').on('click', function(event) {
-    event.preventDefault();
+    // copyright
+    var today = new Date();
+    var year  = today.getFullYear();
+    document.getElementById('copyright').innerHTML = "&copy; " + year;
 
-    var numberGays = $('#numberGays').val();
+    var rainbowIpsum = new gayGenerator();
 
-    // empty container div
-    $('.outcome').empty();
+    $('#gay-generator').on('click', function(event) {
+      event.preventDefault();
+      var numberGays = $('#numberGays').val();
 
-    // append as many paragraphs
-    if ($('#selectGay').val() === 'paragraph') {
-      for (var i = 0; i < numberGays; i++) {
-        $('.outcome').append("<p>" + makeParagraph(5) + "</p>");
-      };
-    }
-    // or words
-    else {
-      $('.outcome').append("<p>" + makeSentence(numberGays) + "</p>");     
-    }
+      // empty container div
+      $('.outcome').empty();
+
+      // append as many paragraphs
+      if ($('#selectGay').val() === 'paragraph') {
+        for (var i = 0; i < numberGays; i++) {
+          $('.outcome').append("<p>" + rainbowIpsum.makeParagraph(5) + "</p>");
+        };
+      }
+      // or sentences
+      else {
+        for (var i = 0; i < numberGays; i++) {
+          $('.outcome').append("<p>" + rainbowIpsum.makeSentence(6) + "</p>");
+        };
+      }
+    });
   });
+})();
 
-});
+
+
